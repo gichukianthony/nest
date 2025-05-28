e360e4dc513b10029682bdd7548eddafa0b33f21
antosnizzah <antonygatitu327@gmail.com>
2025-05-28 21:03:21 +0300

    caling env configurations globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './loger.middleware';
+import { MechanicsModule } from './mechanics/mechanics.module';
+import { FeedbacksModule } from './feedbacks/feedbacks.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MechanicsModule,
+    FeedbacksModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Make the module global
+      envFilePath: '.env', // Path to the environment file
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consume: MiddlewareConsumer) {
