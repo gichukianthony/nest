0f9eb0a2beeb548f76a1374ce7922f5f893d5d48
antosnizzah <antonygatitu327@gmail.com>
2025-06-03 16:11:09 +0300

    global settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,14 @@ import { LoggerMiddleware } from './loger.middleware';
 import { MechanicsModule } from './mechanics/mechanics.module';
 import { FeedbacksModule } from './feedbacks/feedbacks.module';
 import { ServicesModule } from './services/services.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { LogsModule } from './logs/logs.module';
 import { RateLimiterModule } from './rate-limiter/rate-limiter.module';
 import { RateLimiterMiddleware } from './rate-limiter/rate-limiter.middleware';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { CacheableMemory, Keyv } from 'cacheable';
+import { createKeyv } from '@keyv/redis';
 
 @Module({
   imports: [
@@ -23,12 +26,34 @@ import { RateLimiterMiddleware } from './rate-limiter/rate-limiter.middleware';
     ServicesModule,
     LogsModule,
     RateLimiterModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (configService: ConfigService) => ({
+        ttl: 60000,
+        stores: [
+          new Keyv({
+            store: new CacheableMemory({ ttl: 30000, lruSize: 5000 }),
+          }),
+          createKeyv(
+            configService.getOrThrow<string>('REDIS_URL') ||
+              'redis://localhost:6379',
+          ),
+        ],
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: CacheInterceptor,
+    },
   ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware, RateLimiterMiddleware)
-      .forRoutes('*');
+    consumer.apply(LoggerMiddleware, RateLimiterMiddleware).forRoutes('*');
   }
 }
