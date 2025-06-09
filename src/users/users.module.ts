e858cf00158dcc7680781875bd4a6f1606f25340
antosnizzah <antonygatitu327@gmail.com>
2025-06-10 00:13:56 +0300

    users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,10 +4,11 @@ import { UsersController } from './users.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Module({
   imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, RolesGuard],
 })
 export class UsersModule {}
