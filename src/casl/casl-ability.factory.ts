26e61f5f5423d0f8dd8c4d4b20cddf6e7e69046c
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 10:39:02 +0300

    auth factory for claim based

--- src/casl/casl-ability.factory.ts
+++ src/casl/casl-ability.factory.ts
@@ -1,27 +1,26 @@
 import { AbilityBuilder, PureAbility } from '@casl/ability';
 import { Action } from './action.enum';
 import { Role, User } from 'src/users/entities/user.entity';
+import { Injectable } from '@nestjs/common';
 
 type Subject = 'users' | 'feedbacks' | 'mechanics' | 'services' | 'all';
 
 export type AppAbility = PureAbility<[Action, Subject]>;
 
-@injectable()
+@Injectable()
 export class CaslAbilityFactory {
-    createForUser(user: User): AppAbility{
-        const {can, build} = new AbilityBuilder<AppAbility>(PureAbility);
+  createForUser(user: User): AppAbility {
+    const { can, build } = new AbilityBuilder<AppAbility>(PureAbility);
 
-        if(user.role === Role.ADMIN){
-            can(Action.Manage, 'all')
-        }
-        else if (user.role === Role.USER){
-            can(Action.Read,[
-                'feedbacks',
-                'mechanics',
-                'services',
-            ])
-            can(Action.Create, 'feedbacks');
-
-        }else if (user.role === Role.MECHANIC){
-            can(Action.Read,[])
+    if (user.role === Role.ADMIN) {
+      can(Action.Manage, 'all');
+    } else if (user.role === Role.USER) {
+      can(Action.Read, ['feedbacks', 'mechanics', 'services']);
+      can(Action.Create, 'feedbacks');
+    } else if (user.role === Role.MECHANIC) {
+      can(Action.Read, ['feedbacks', 'mechanics', 'services']);
+      can(Action.Update, 'services', { mechanicId: user.id });
     }
+    return build();
+  }
+}
