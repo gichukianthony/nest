a856ef68e9d4995e10f6a895371cf3109ca8604d
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 11:08:33 +0300

    emails transporter

---
+++ src/mails/mail.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class MailService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async sendMail(options: {
+    to: string;
+    subject: string;
+    text?: string;
+    html?: string;
+  }): Promise<void> {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: options.to,
+      subject: options.subject,
+      text: options.text,
+      html: options.html,
+    };
+
+    await transporter.sendMail(mailOptions);
+  }
+}
