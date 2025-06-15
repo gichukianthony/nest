7a6e4631a9febd1dd4efcf0d5cba139df11722df
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 10:51:05 +0300

    mechanic module

--- src/mechanics/mechanics.module.ts
+++ src/mechanics/mechanics.module.ts
@@ -3,11 +3,13 @@ import { MechanicsService } from './mechanics.service';
 import { MechanicsController } from './mechanics.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Mechanic } from './entities/mechanic.entity';
-import { User } from 'src/users/entities/user.entity';
+import { User } from '../users/entities/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Mechanic, User])],
+  imports: [TypeOrmModule.forFeature([Mechanic, User]), AuthModule],
   controllers: [MechanicsController],
   providers: [MechanicsService],
+  exports: [MechanicsService],
 })
 export class MechanicsModule {}
