459708e6b44f31f61f0117fb18c01ee34e41658b
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 10:54:12 +0300

    updating validation

--- src/services/dto/update-service.dto.ts
+++ src/services/dto/update-service.dto.ts
@@ -1,4 +1,57 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateServiceDto } from './create-service.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateServiceDto extends PartialType(CreateServiceDto) {}
+export class UpdateServiceDto extends PartialType(CreateServiceDto) {
+  @ApiProperty({
+    description: 'The name of the service',
+    example: 'Oil Change',
+    required: false,
+  })
+  name?: string;
+
+  @ApiProperty({
+    description: 'The description of the service',
+    example: 'Complete oil change service including filter replacement',
+    required: false,
+  })
+  description?: string;
+
+  @ApiProperty({
+    description: 'The price of the service',
+    example: 49.99,
+    minimum: 0,
+    required: false,
+  })
+  price?: number;
+
+  @ApiProperty({
+    description: 'The duration of the service in minutes',
+    example: 60,
+    minimum: 1,
+    required: false,
+  })
+  duration?: number;
+
+  @ApiProperty({
+    description: 'Whether the service is currently available',
+    example: true,
+    required: false,
+  })
+  isAvailable?: boolean;
+
+  @ApiProperty({
+    description: 'The ID of the service category',
+    example: 1,
+    required: false,
+  })
+  categoryId?: number;
+
+  @ApiProperty({
+    description: 'Array of mechanic IDs who can perform this service',
+    example: [1, 2, 3],
+    required: false,
+    type: [Number],
+  })
+  mechanicIds?: number[];
+}
