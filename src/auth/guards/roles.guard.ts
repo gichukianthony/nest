764c40b15612143d86b761b6bb749a890e333d7d
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 10:36:13 +0300

    authorization guard

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -1,44 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Role, User } from 'src/users/entities/user.entity';
-import { Repository } from 'typeorm';
+import { Role } from '../../users/entities/user.entity';
 import { ROLES_KEY } from '../decorators/roles.decorator';
-import { Request } from 'express';
-import { JWTPayload } from '../strategies/at.strategy';
 
-interface UserRequest extends Request {
-  user?: JWTPayload;
-}
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-    @InjectRepository(User)
-    private userRepository: Repository<User>,
-  ) {}
+  constructor(private reflector: Reflector) {}
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
+  canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
+
     if (!requiredRoles) {
-      return true; // If no roles are required, allow access
+      return true;
     }
-    const request = context.switchToHttp().getRequest<UserRequest>();
-    const user = request.user;
+
+    const { user } = context.switchToHttp().getRequest();
 
     if (!user) {
-      return false; // If no user is found in the request, deny access
+      return false;
     }
-    const userprofile = await this.userRepository.findOne({
-      where: { id: user.sub },
-      select: ['id', 'role'],
-    });
-    if (!userprofile) {
-      return false; // If user profile is not found, deny access
-    }
-    return requiredRoles.some((role) => role === userprofile.role);
+
+    return requiredRoles.some((role) => user.role === role);
   }
 }
