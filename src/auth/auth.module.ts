117835797fd29662fe9a8bc84cf6f3510d962f68
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 10:34:05 +0300

    auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,17 +8,32 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
 import { AtStrategy } from './strategies/at.strategy';
 import { RfStrategy } from './strategies/rt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AtGuard } from './guards/at.guard';
+import { RolesGuard } from './guards/roles.guard';
+import { MailModule } from '../mails/mail.module';
 
 @Module({
   imports: [
     DatabaseModule,
     TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      global: true,
-    }), // Register JwtModule with global configuration
-    PassportModule, // Register PassportModule for strategies
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_ACCESS_TOKEN_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<string>(
+            'JWT_ACCESS_TOKEN_EXPIRATION_TIME',
+          ),
+        },
+      }),
+    }),
+    PassportModule.register({ defaultStrategy: 'jwt-at' }),
+    MailModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService, AtStrategy, RfStrategy],
+  providers: [AuthService, AtStrategy, RfStrategy, AtGuard, RolesGuard],
+  exports: [AtGuard, RolesGuard],
 })
 export class AuthModule {}
