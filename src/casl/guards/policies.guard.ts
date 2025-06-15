866aae4e9d617124a46d4225ec6d4163a84d7587
antosnizzah <antonygatitu327@gmail.com>
2025-06-15 12:31:09 +0300

    app abiblity type updated

--- src/casl/guards/policies.guard.ts
+++ src/casl/guards/policies.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { CaslAbilityFactory } from '../casl-ability.factory';
+import { AppAbility, CaslAbilityFactory } from '../casl-ability.factory';
 import { CHECK_POLICIES_KEY } from '../decorators/check-policies.decorator';
 import { PolicyHandler } from '../interfaces/policy-handler.interface';
 
@@ -34,7 +34,7 @@ export class PoliciesGuard implements CanActivate {
     );
   }
 
-  private execPolicyHandler(handler: PolicyHandler, ability: any) {
+  private execPolicyHandler(handler: PolicyHandler, ability: AppAbility) {
     if (typeof handler === 'function') {
       return handler(ability);
     }
